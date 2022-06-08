599fd6746ab59e428f54b3974a1c2b0c928c0cd7
Mariia <malitvia@gmail.com>
2022-06-08 15:52:15 -0400

    Updates

---
+++ precache-manifest.a5fd2a7ef9a0b4149fd1c72f0c982cb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3489d030c3bd01b124a6",
+    "url": "/Robofriends/static/css/main.78acc0f0.chunk.css"
+  },
+  {
+    "revision": "3489d030c3bd01b124a6",
+    "url": "/Robofriends/static/js/main.3489d030.chunk.js"
+  },
+  {
+    "revision": "74c32f05c50e043acc40",
+    "url": "/Robofriends/static/css/1.a8d2752f.chunk.css"
+  },
+  {
+    "revision": "74c32f05c50e043acc40",
+    "url": "/Robofriends/static/js/1.74c32f05.chunk.js"
+  },
+  {
+    "revision": "39a3a35c71e0df973673",
+    "url": "/Robofriends/static/js/runtime~main.39a3a35c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "31bfb20c37134cf75eefae164c067638",
+    "url": "/Robofriends/index.html"
+  }
+];
